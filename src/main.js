56b6802a454dd2c5bff3da75a9dd23cc60ca82eb
Ameya <ameyakool3@yahoo.com>
2025-07-26 23:18:28 +0530

    added animation

--- src/main.js
+++ src/main.js
@@ -39,3 +39,21 @@ closeBtn.addEventListener("click", () => {
   youtubeFrame.src = "";
   pipPlayer.classList.add("hidden");
 });
+
+const sections = document.querySelectorAll(".fade-in-section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+sections.forEach(section => observer.observe(section));
