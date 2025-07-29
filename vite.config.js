494caeaa411b331285bf2bb1a400690325e161fc
Ameya <ameyakool3@yahoo.com>
2025-07-29 20:37:10 +0530

    Added new pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    base: '/the-human-spectrum-2.0/'
-})
+  base: "/the-human-spectrum-2.0/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        collaboration: resolve(__dirname, "collaboration.html"),
+        intervention: resolve(__dirname, "intervention.html"),
+        mannmade: resolve(__dirname, "mannmade.html"),
+        awareness: resolve(__dirname, "awareness.html"),
+      },
+    },
+  },
+});
